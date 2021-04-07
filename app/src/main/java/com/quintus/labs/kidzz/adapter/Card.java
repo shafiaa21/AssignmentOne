e257b15d09ccd80ec5a7332a9f96a2fac2926114
Shafiaa21 <bsef18m030@pucit.edu.pk>
2021-04-07 20:46:38 +0500

    AssignmentOne

---
+++ app/src/main/java/com/quintus/labs/kidzz/adapter/Card.java
@@ -0,0 +1,18 @@
+package com.quintus.labs.kidzz.adapter;
+
+import android.widget.Button;
+
+public class Card {
+
+    public int x;
+    public int y;
+    public Button button;
+
+    public Card(Button button, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.button = button;
+    }
+
+
+}
---
+++ app/src/main/java/com/quintus/labs/kidzz/fragment/DrawingFragment.java
@@ -0,0 +1,32 @@
+package com.quintus.labs.kidzz.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.quintus.labs.kidzz.R;
+
+
+public class DrawingFragment extends Fragment {
+
+
+    public DrawingFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_drawing, container, false);
+
+        return view;
+    }
+
+
+}
